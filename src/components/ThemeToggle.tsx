26e53b4afbf2807ea59c3114c371bfd7a07e7a9b
viz-an <anirudh.viswanath8@gmail.com>
2025-03-07 14:19:28 +0000

    fix: vercel fix

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Moon, Sun, Laptop } from "lucide-react";
